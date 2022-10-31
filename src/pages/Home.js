8dd531f867ccdd3e15d7321a22f86da91b4283f8
basit-dev <bas677@icloud.com>
2022-10-31 22:41:45 +0000

    Navigation header created
    Nav bar on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,35 @@ export default function Home() {
   return (
     <>
       <div>Home</div>
-      <Link to="/">Home</Link>
-      <Link to="/product">Product</Link>
-      <Link to="/cart">Cart</Link>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+        <Link to="/" className="navbar-brand">Home</Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+              <Link to="/product">Product</Link>
+              </li>
+              <li class="nav-item">
+              <Link to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+   
+
+
     </>
   );
 }
