fa616f444d5b481098f549441fd6d839e4c4d326
basit-dev <bas677@icloud.com>
2022-10-31 22:01:43 +0000

    Added react-router-dom
    Configured default routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <h1>App</h1>
-    </div>
+    <Routes>
+      <Route index element={null} />
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+    </Routes>
   );
 }
 
